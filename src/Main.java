4bf459e77c7136928b1f6e17358fae5134fcb907
KosNet75 <nemokonstantin@gmail.com>
2022-08-16 18:53:22 +0700

    commit3

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,13 @@ public class Main {
     person.add(new Person("Владимир", "Немирович-Данченко", 84));
     person.add(new Person("Александр", "Голенищев-Кутузов-Смоленский", 67));
     person.add(new Person("Александр", "Панкратов-Чёрный", 73));
-    person.add(new Person("Elizabeth II Alexandra Mary", "Windsor", 96));
+    person.add(new Person("Elizabeth II", "Alexandra-Mary-Windsor", 96));
     person.add(new Person("Пётр", "Румянцев-Задунайский", 71));
     person.add(new Person("Елизавета", "Боярская", 36));
     person.add(new Person("Lindsay", "Lohan", 36));
 
     //Collections.sort(person, new ComparePerson<>(4));
-    person.sort(new ComparePerson<>(4));
+    person.sort(new ComparePerson<>(3));
     System.out.println();
     System.out.println(person);
 
