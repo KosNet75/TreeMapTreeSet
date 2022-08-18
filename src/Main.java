637411b24e891dbf10c56bff44d70b0f13913808
KosNet75 <nemokonstantin@gmail.com>
2022-08-18 11:21:30 +0700

    commit Final

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 
 public class Main {
 
   public static void main(String[] args) {
 
-    LinkedList<Person> person = new LinkedList<>();
+   // LinkedList<Person> person = new LinkedList<>();
+    TreeSet<Person> person = new TreeSet<>(new ComparePerson<>();
+    //NavigableMap<Person, Person> worldCupMap = new TreeMap<>();
 
     person.add(new Person("Владимир", "Немирович-Данченко", 84));
     person.add(new Person("Александр", "Голенищев-Кутузов-Смоленский", 67));
@@ -18,8 +23,17 @@ public class Main {
     person.add(new Person("Анна", "Цуканова-Котт", 33));
 
     //Collections.sort(person, new ComparePerson<>(4));
-    person.sort(new ComparePerson<>(3));
+    //person.sort(new ComparePerson<>(1));
     System.out.println();
     System.out.println("Рейтинг по убыванию, сверху вниз: " + person);
+
+
+
+
+
+
+
+
+
   }
 }
