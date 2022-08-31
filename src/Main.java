c63ad029cc37f77204a6616e6ebac4490d6e139c
KosNet75 <nemokonstantin@gmail.com>
2022-08-31 21:24:48 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Comparator;
 import java.util.NavigableSet;
 import java.util.TreeSet;
 
@@ -6,27 +5,9 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Comparator<Person> sortPerson = (o1, o2) -> {
-
-      int maxSize = 3;
-      String[] words1 = o1.getSurname().split("\\P{IsAlphabetic}+");
-      int wordCount1 = words1.length;
-      String[] words2 = o2.getSurname().split("\\P{IsAlphabetic}+");
-      int wordCount2 = words2.length;
-      if (wordCount1 > maxSize) {
-        wordCount1 = maxSize;
-      }
-      if (wordCount2 > maxSize) {
-        wordCount2 = maxSize;
-      }
-
-      if (wordCount1 > wordCount2 || wordCount1 < wordCount2) {
-        return wordCount1 > wordCount2 ? 1 : -1;
-      }
-      return Integer.compare(o1.getAge(), o2.getAge());
-    };
-
+    ComparePerson sortPerson = new ComparePerson(3);
     TreeSet<Person> person = new TreeSet<>(sortPerson);
+
     person.add(new Person("Владимир", "Немирович-Данченко", 84));
     person.add(new Person("Александр", "Голенищев-Кутузов-Смоленский", 67));
     person.add(new Person("Elizabeth II", "Alexandra-Mary-Windsor", 96));
