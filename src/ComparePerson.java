48a9b7528a41805519f9a4f5c3997bfda0419369
KosNet75 <nemokonstantin@gmail.com>
2022-08-31 21:21:23 +0700

    1

---
+++ src/ComparePerson.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ComparePerson implements Comparator<Person> {
+
+  int maxSize;
+
+  public ComparePerson(int maxSize) {
+    this.maxSize = maxSize;
+  }
+
+  @Override
+  public int compare(Person o1, Person o2) {
+
+    String[] words1 = o1.getSurname().split("-");
+    int wordCount1 = words1.length;
+    String[] words2 = o2.getSurname().split("-");
+    int wordCount2 = words2.length;
+
+    if (wordCount1 > maxSize) {
+      wordCount1 = maxSize;
+    }
+    if (wordCount2 > maxSize) {
+      wordCount2 = maxSize;
+    }
+
+    if (wordCount1 > wordCount2 || wordCount1 < wordCount2){
+      return wordCount1 > wordCount2 ? 1 : -1;
+    }
+
+    return Integer.compare(o1.getAge(), o2.getAge());
+
+  }
+}
